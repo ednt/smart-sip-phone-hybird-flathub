c3cc24bd88a63f3a0bc19e5c55aaf4a1659b031f
sr <sr@debian.ednt.de>
2026-06-26 09:10:50 +0200

    Release 1.7.0

--- smart-sip-phone-launch.sh
+++ smart-sip-phone-launch.sh
@@ -7,5 +7,7 @@ mkdir -p "${HOME}/.${PROGRAM_NAME}"
 touch "${HOME}/.${PROGRAM_NAME}/extract_on_next_start"
 export WEBKIT_DISABLE_DMABUF_RENDERER="${WEBKIT_DISABLE_DMABUF_RENDERER:-1}"
 export GDK_BACKEND="${GDK_BACKEND:-x11}"
+export PATH="/app/bin:${PATH}"
+export LD_LIBRARY_PATH="/app/lib${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}"
 
 exec /app/bin/"${PROGRAM_NAME}" "$@"
